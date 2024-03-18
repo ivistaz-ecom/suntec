dc0cafe971fa72d469bf972936ad228b1933f83c
Shubham Shree <shubhamshree@iVMBA-M2-09.localdomain>
2024-03-18 14:08:59 +0530

    Product section: Responsiveness /products

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "470cc39860b81963"; }
+/******/ 		__webpack_require__.h = function() { return "d8ce6e4bcc296f86"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
